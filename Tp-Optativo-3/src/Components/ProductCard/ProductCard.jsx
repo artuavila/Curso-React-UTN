a1ea6c349e4d9d2987c6819d2e9c6f6db518e213
artuavila <81383302+artuavila@users.noreply.github.com>
2025-04-23 20:35:52 -0300

    Add files via upload

---
+++ Tp-Optativo-3/src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Button from '../Button/Button'
+import { Link } from 'react-router-dom'
+import './ProductCard.css'
+
+const ProductCard = ({img, title, real_price, final_price, discount, stock, id}) => {
+    return (
+        <div className='producto'>
+            <img className='imagen' 
+                src={img} />
+                <div className='div-titulo'>
+                    <span className='titulo'>{title}</span>
+                </div>
+            <div className='div-precios'>
+                <div className='div-pre-desc'>
+                    <span className='precio'>${real_price}</span>
+                    <span className='descuento'>{discount}% OFF</span>
+                </div>
+                <span className='preciofinal'>${final_price}</span>
+            </div>
+            <span className='cantidad'>{stock} disponibles</span>
+            <Button />
+            <Link className='link-ver-mas' 
+                to={`/product/${id}`}>
+                    <div className='div-ver-mas'>Ver mas</div>
+            </Link>
+        </div>
+    )
+}
+
+export default ProductCard
